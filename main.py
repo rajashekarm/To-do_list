e86fccdaaf5578591c6e79d07ee8390cce95d1ec
rajashekarm <94944999+rajashekarm@users.noreply.github.com>
2023-08-25 23:24:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,35 @@
 import streamlit as st
+import sqlite3
+from hashlib import sha256
 
-# Mock user data for authentication
-users = {
-    "user1": "password1",
-    "user2": "password2",
-}
+# Create a SQLite database
+conn = sqlite3.connect("todo.db")
+c = conn.cursor()
+c.execute('''
+    CREATE TABLE IF NOT EXISTS users (
+        username TEXT PRIMARY KEY,
+        password TEXT
+    )
+''')
+c.execute('''
+    CREATE TABLE IF NOT EXISTS tasks (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT,
+        task TEXT
+    )
+''')
+conn.commit()
+
+def signup():
+    st.subheader("Sign Up")
+    new_username = st.text_input("New Username")
+    new_password = st.text_input("New Password", type="password")
+
+    if st.button("Sign Up"):
+        hashed_password = sha256(new_password.encode()).hexdigest()
+        c.execute("INSERT INTO users (username, password) VALUES (?, ?)", (new_username, hashed_password))
+        conn.commit()
+        st.success("Account created successfully. You can now log in.")
 
 def login():
     st.subheader("Login")
@@ -12,23 +37,43 @@ def login():
     password = st.text_input("Password", type="password")
 
     if st.button("Login"):
-        if username in users and users[username] == password:
+        hashed_password = sha256(password.encode()).hexdigest()
+        c.execute("SELECT * FROM users WHERE username = ? AND password = ?", (username, hashed_password))
+        user = c.fetchone()
+        if user:
             return True
         else:
             st.error("Invalid username or password")
     return False
 
+def add_task(username):
+    st.subheader("Add Task")
+    task = st.text_input("Enter a task:")
+    if st.button("Add Task"):
+        c.execute("INSERT INTO tasks (username, task) VALUES (?, ?)", (username, task))
+        conn.commit()
+        st.success("Task added!")
+
+def show_tasks(username):
+    st.subheader("Your Tasks")
+    c.execute("SELECT task FROM tasks WHERE username = ?", (username,))
+    tasks = c.fetchall()
+    for task in tasks:
+        st.write("- " + task[0])
+
 def main():
     st.title("To-Do List App")
-    st.write("Please log in to access your tasks.")
-
+    
+    st.sidebar.header("Authentication")
+    signup()
     authenticated = login()
 
     if authenticated:
-        st.success("Logged in successfully!")
-        tasks = st.text_area("Enter your task:")
-        if st.button("Add Task"):
-            st.write(f"Added task: {tasks}")
+        st.sidebar.success("Logged in successfully!")
+        st.sidebar.subheader("Task Management")
+        username = st.sidebar.text_input("Username", key="username")
+        add_task(username)
+        show_tasks(username)
 
 if __name__ == "__main__":
     main()
